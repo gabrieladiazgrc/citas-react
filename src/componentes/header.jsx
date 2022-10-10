2c7ca66501e3e0155728c1fad3a798ef29ebb7f4
Noni Díaz <ada_hack130@hackademy.mx>
2022-10-10 11:48:41 -0500

    Componentes Header y ListadoPacientes

---
+++ src/componentes/header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>¡Hola, mundo!</h1>
+      )
+}
+
+export default Header;
---
+++ src/componentes/listadoPaciente.jsx
@@ -0,0 +1,7 @@
+function ListadoPaciente() {
+    return (
+        <h2>Listado Paciente</h2>
+      )
+}
+
+export default ListadoPaciente;
