16407224872d1b9ba6647f51478b1caccff1f80d
Noni Díaz <ada_hack130@hackademy.mx>
2022-10-13 12:04:00 -0500

    Estilos Header y Formulario

--- src/componentes/formulario.jsx
+++ src/componentes/formulario.jsx
@@ -1,6 +1,9 @@
 function Formulario() {
     return(
-        <h2>Formulario</h2>
+        <div>
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+            <p className="mt-5xl text-center text-lg">Añade pacientes <span className="text-indigo-600">Administrarlos</span></p>
+        </div>
     )
 }
 
